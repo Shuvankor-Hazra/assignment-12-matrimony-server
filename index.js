e65820ac20697ce3e5e6ec80b380a87007619c36
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-06-12 14:31:15 +0600

    my contact delete btn added

--- index.js
+++ index.js
@@ -248,6 +248,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/contactRequest/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await contactRequestCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // payment related------------------------
     app.post("/create-payment-intent", async (req, res) => {
       const { price } = req.body;
